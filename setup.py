6540713af5225866b380f28c27561f05ceb2dc57
Mark Story <mark@mark-story.com>
2016-11-06 10:49:36 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ This extension provides a PHP domain for sphinx
 
 setup(
     name='sphinxcontrib-phpdomain',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/markstory/sphinxcontrib-phpdomain',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-phpdomain',
     license='BSD',
