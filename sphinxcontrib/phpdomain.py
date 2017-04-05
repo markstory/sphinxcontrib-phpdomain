cd0dd7276f48499f3b37d37c0b3e0ed809843ee1
Mark Story <mark@mark-story.com>
2017-04-04 21:39:05 -0400

    Replace iteritems() with items()
    iteritems() doesn't exist in py3.

--- sphinxcontrib/phpdomain.py
+++ sphinxcontrib/phpdomain.py
@@ -486,7 +486,7 @@ class PhpNamespaceIndex(Index):
         ignores = self.domain.env.config['modindex_common_prefix']
         ignores = sorted(ignores, key=len, reverse=True)
         # list of all modules, sorted by module name
-        modules = sorted(self.domain.data['namespaces'].iteritems(),
+        modules = sorted(self.domain.data['namespaces'].items(),
                          key=lambda x: x[0].lower())
         # sort out collapsable modules
         prev_modname = ''
@@ -535,7 +535,7 @@ class PhpNamespaceIndex(Index):
         collapse = len(modules) - num_toplevels < num_toplevels
 
         # sort by first letter
-        content = sorted(content.iteritems())
+        content = sorted(content.items())
 
         return content, collapse
 
@@ -678,9 +678,9 @@ class PhpDomain(Domain):
         return newname, objects[newname]
 
     def get_objects(self):
-        for ns, info in self.data['namespaces'].iteritems():
+        for ns, info in self.data['namespaces'].items():
             yield (ns, ns, 'namespace', info[0], 'namespace-' + ns, 0)
-        for refname, (docname, type) in self.data['objects'].iteritems():
+        for refname, (docname, type) in self.data['objects'].items():
             yield (refname, refname, type, docname, refname, 1)
 
 
